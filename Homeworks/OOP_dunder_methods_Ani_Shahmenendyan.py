7d2f887cc1c3acd086b89dd1a70d3adb6eb22903
AniShahmenendyan <ashahmenendian@gmail.com>
2022-02-22 18:18:01 +0400

    secon commit

--- Homeworks/OOP_dunder_methods_Ani_Shahmenendyan.py
+++ Homeworks/OOP_dunder_methods_Ani_Shahmenendyan.py
@@ -14,39 +14,46 @@ class Integer:
             return self._n
 
     def __add__(self, other):
-        return Integer(self._n + other.get_n())
+        if type(other) == Integer:
+            return Integer(self._n + other.get_n())
+
 
     def __sub__(self, other):
-        return Integer(self._n - other.get_n())
+        if type(other) == Integer:
+            return Integer(self._n - other.get_n())
+
 
     def __mul__(self, other):
-        return Integer(self._n * other.get_n())
+        if type(other) == Integer:
+            return Integer(self._n * other.get_n())
+
 
     def __truediv__(self, other):
-        if other.get_n != 0:
-            return Integer(self._n / other.get_n())
-        return Inf(self._n / other.get_n())
+        if type(other) == Integer and other.get_n() != 0:
+            return self._n / other.get_n()
+        else:
+            return Inf
 
     def __eq__(self, other):
-        return self._n == other.get_n()
+        return self._n == other._n
 
     def __ne__(self, other):
         return not self == other
 
     def __lt__(self, other):
-        return self._n < other.get_n()
+        return self._n < other._n
 
     def __le__(self, other):
-        return self._n <= other.get_n()
+        return self._n <= other._n
 
     def __gt__(self, other):
-        return self._n > other.get_n()
+        return self._n > other._n
 
     def __ge__(self, other):
-        return self._n >= other.get_n()
+        return self._n >= other._n
 
-    # def __str__(self):
-    #     return f'{self._n}'
+    def __str__(self):
+        return f'{self._n}'
 
     def __bool__(self):
         if self._n != 0:
@@ -65,22 +72,24 @@ b = Integer(2)
 
 print(a + b)
 print(a - b)
-# print(a * b)
-# print(a / b)
-# print(a == b)
-# print(a < b)
-# print(a <= b)
-# print(a > b)
-# print(a >= b)
+print(a * b)
+print(a / b)
+print(a == b)
+print(a < b)
+print(a <= b)
+print(a > b)
+print(a >= b)
 print(bool(a))
 print('------------------')
 
-
 class Colors:
     def __init__(self, r, g, b):
-        self.__red = r
-        self.__green = g
-        self.__blue = b
+        if r in range(256) and g in range(256) and b in range(256):
+            self.__red = r
+            self.__green = g
+            self.__blue = b
+        else:
+            raise ValueError
 
     @property
     def r(self):
@@ -115,19 +124,25 @@ class Colors:
         else:
             raise ValueError
 
-    def __add__(self, other):
-        return Colors(self.__red + other.r(), self.__green + other.g(), self.__blue + other.b())
+    # def __add__(self, other): # սա չի աշխատում
+    #     if type(other) == Colors:
+    #         return Colors(self.__red + other.r(), self.__green + other.g(), self.__blue + other.b())
 
     def rgb_to_hex(self):
         return f'Red is:{hex(self.__red)},Green is: {hex(self.__green)},Blue is: {self.__blue}'
 
     def __str__(self):
-        return f'Red is: {self.__red} \nBlue is: {self.__blue} \nGreen is: {self.__green}'
+        return f'Red is: {self.__red} \nGreen is: {self.__green} \nBlue is: {self.__blue}'
 
+    # def show_my_color(self):
+    #     return color_front(f'This is my color: {self.__red}, {self.__green}, {self.__blue})
 
-clr = Colors(0, 260, 1)
-new_clr = Colors(20, -50, 200)
+clr = Colors(0, 220, 1)
+clr2 = Colors(50,0,20)
+
+# new_clr = Colors(clr + clr2)
 
 print(clr)
-# print(clr + new_clr) #  սա չի աշխատում
+print ('-----------')
+# print(new_clr)
 print('----------------')
