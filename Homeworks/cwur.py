37176c63e3be96446afb53658763bd410ab8172f
AniShahmenendyan <ashahmenendian@gmail.com>
2022-04-10 20:11:44 +0400

    update homework

--- Homeworks/cwur.py
+++ Homeworks/cwur.py
@@ -12,22 +12,26 @@ url = "https://cwur.org/2021-22.php"
 response = requests.get(url, headers=headers)
 if response.status_code == 200:
     response_html = response.text
-    with open('../web/universities.csv', 'w') as file:
-        file.write(response_html)
-        soup = BeautifulSoup(response_html, 'html.parser')
+    # with open('../web/universities.csv', 'w') as file:
+    #     file.write(response_html)
+    soup = BeautifulSoup(response_html, 'html.parser')
 
 
 
-university_list_div = soup.find('div', {'class': 'table-responsive'})
-university_name_list = university_list_div.find_all('a')
+
+university_list_div = soup.find('div', {'class': 'table-responsive'}).find_all('td')
+# print(university_list_div)
 universities_list = []
-for name in university_name_list:
-    universities_list.append(name.text)
-i = 1
-while  i < len(universities_list):
-    universities_list.pop(i)
-    i += 1
-print(universities_list)
+
+for val in university_list_div:
+    universities_list.append(val.text)
+# print(universities_list)
+universities = []
+for i in range(0, len(universities_list)-9, 9):
+    universities.append({'id': universities_list[i], 'name': universities_list[i + 1],
+                        'country': universities_list[i + 2],
+                        'score': universities_list[i+8]})
+
 
 with open('../web/universities.json', 'w') as f:
-    f.write(json.dumps(universities_list, ensure_ascii=False))
+    f.write(json.dumps(universities, ensure_ascii=False))
